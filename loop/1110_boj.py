192a8ace3722cb33a469505097aa6c0b4210baed
HongTae Kim <y159159@naver.com>
2022-08-25 01:34:12 +0900

    20220825

--- loop/1110_boj.py
+++ loop/1110_boj.py
@@ -3,13 +3,11 @@ num = n
 cnt = 0
 
 while True:
-    a = num // 10
-    b = num % 10
-    c = (a+b) % 10
-    num = (b*10) + c
-
     cnt = cnt + 1
-    if(num==n):
+    a = num % 10    # a : 주어진 수의 오른쪽 자리 수
+    b = num // 10   # b : 주어진 수의 왼쪽 자리 수
+    add = (a + b) % 10
+    num = (a * 10) + add
+    if num == n:
         break
-
 print(cnt)
