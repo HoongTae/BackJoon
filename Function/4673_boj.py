7dafcb2f369466ce617f081ebb897e25478bafcf
HongTae Kim <y159159@naver.com>
2022-08-27 23:47:22 +0900

    20220827

--- Function/4673_boj.py
+++ Function/4673_boj.py
@@ -1,8 +1,27 @@
-def self_num(n):
+def not_self_num(n):
     self = n
-    for i in range(6):
-        if  
-        remainder = self % 10
-        self += remainder
+    remainder = n
+    while True:
+        self += remainder % 10
+        remainder //= 10
+        if remainder == 0:
+            break
+    return self
 
 
+list_no = []
+list_nsn = []
+for i in range(1, 10001):
+    list_no.append(i)
+for i in range(1, 10001):
+    list_nsn.append(not_self_num(i))
+    if not_self_num(i) > 10000:
+        list_nsn.remove(not_self_num(i))
+
+list_nsn = sorted(list(set(list_nsn)))
+
+for i in range(len(list_nsn)):
+    list_no.remove(list_nsn[i])
+
+for j in range(len(list_no)):
+    print(list_no[j])
