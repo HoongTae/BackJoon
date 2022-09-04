3a19cada710e3ce164ff4835307b5dfa1495022e
HongTae Kim <y159159@naver.com>
2022-09-04 19:45:08 +0900

    20220904

---
+++ String/10809_boj.py
@@ -0,0 +1,6 @@
+S = input()
+alpha = list(range(97, 123))
+
+for i in alpha:
+    print(S.find(chr(i)), end=' ')
+    
---
+++ String/11720_boj.py
@@ -0,0 +1,8 @@
+N = int(input())
+num = input()
+
+s = 0
+for n in num:
+    s += int(n)
+
+print(s)
