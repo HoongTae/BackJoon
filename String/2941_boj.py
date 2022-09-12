58ab8b40522226f26a0af4daead89319a393bc04
HongTae Kim <y159159@naver.com>
2022-09-12 22:59:12 +0900

    20220912

--- String/2941_boj.py
+++ String/2941_boj.py
@@ -2,21 +2,26 @@ S = list(input())
 cnt = 0
 
 for i in range(len(S)):
-    if S[i] == 'c' or S[i] == 's' or S[i] == 'z':
-        if S[i+1] == '=' or S[i+1] == '-':
-            continue
-    elif S[i] == 'd':
-        if i+2 > len(S):
-            break
-        elif S[i+1] == 'z' and S[i+2] == '=':
-            continue
-        elif S[i+1] == '-':
-            continue
-    elif S[i] == 'l' or S[i] == 'n':
-        if S[i+1] == 'j':
-            continue
-    if i == len(S):
-        break
-    cnt += 1
+    if i == (len(S)-1):
+        cnt += 1
+    else:
+        if S[i] == 'c' or S[i] == 's' or S[i] == 'z':
+            if S[i + 1] == '=' or S[i + 1] == '-':
+                continue
+        elif S[i] == 'd':
+            if i + 2 > (len(S)-1):
+                cnt += 1
+                continue
+            elif S[i + 1] == 'z' and S[i + 2] == '=':
+                continue
+            elif S[i + 1] == '-':
+                continue
+        elif S[i] == 'l' or S[i] == 'n':
+            if S[i + 1] == 'j':
+                continue
+        cnt += 1
 
 print(cnt)
+
+
+# ddz=z=jdz
