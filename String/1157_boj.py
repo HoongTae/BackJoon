2f9fdfedcd2244e6d63631f7b8ad2904e89b4a6b
HongTae Kim <y159159@naver.com>
2022-09-19 19:35:06 +0900

    20220919

--- String/1157_boj.py
+++ String/1157_boj.py
@@ -1,12 +1,26 @@
 word = input().upper()
-word_list = list(set(word))
+unique_word = list(set(word))
 
-cnt = []
-for i in word_list:
-    count = word.count
-    cnt.append(count(i))
+word_cnt = []
+for uw in unique_word:
+    word_cnt.append(word.count(uw))
 
-if cnt.count(max(cnt)) > 1:
+if word_cnt.count(max(word_cnt)) > 1:
     print("?")
 else:
-    print(word_list[(cnt.index(max(cnt)))])
+    print(unique_word[word_cnt.index(max(word_cnt))])
+
+
+# word = input().upper()
+# word_list = list(set(word))
+#
+# cnt = []
+# for i in word_list:
+#     count = word.count
+#     cnt.append(count(i))
+#
+# if cnt.count(max(cnt)) > 1:
+#     print("?")
+# else:
+#     print(word_list[(cnt.index(max(cnt)))])
+#
